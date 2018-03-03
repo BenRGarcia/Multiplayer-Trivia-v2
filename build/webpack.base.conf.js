1af07419844b366bbfc7050802ef0ce27c9a42e0
Ben Garcia <ben@seebenprogram.com>
2018-03-03 17:23:28 -0500

    Add jquery/popper.js with ProvidePlugin

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -67,6 +68,13 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+      new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Popper: ['popper.js', 'default'] 
+    })
+  ],
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
