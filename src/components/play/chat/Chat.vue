2cc6cc21a5fd602ae2b4414919ef5a85259c469d
Ben Garcia <ben@seebenprogram.com>
2018-03-03 22:52:18 -0500

    Build component shell for subcomponents

--- src/components/play/chat/Chat.vue
+++ src/components/play/chat/Chat.vue
@@ -1,13 +1,52 @@
 <template>
-  
+  <div class="card p-2">
+    <div id="chat" class="card-body border border-secondary rounded">
+
+      <!-- Chat History loads from db to here -->
+      <ChatHistory/>
+
+    </div>
+    <div class="card-footer">
+      
+      <!-- User sends new chat msg here -->
+      <ChatInput/>
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import ChatHistory from './ChatHistory'
+import ChatInput from './ChatInput'
 
+export default {
+  components: {
+    ChatHistory,
+    ChatInput
+  },
+  // Lifecycle hooks, scroll chat to bottom
+  mounted: function() {
+    $('#chat').scrollTop( 1000 );
+  },
+  updated: function () {
+    $('#chat').scrollTop( 1000 );
+  }
 }
 </script>
 
 <style scoped>
-
+.card-body {
+  height: 200px;
+  overflow: scroll;
+  padding-bottom: 0;
+}
+.card-footer {
+  padding: 0.5rem 0 0 0;
+}
+div {
+  text-align: left;
+}
+hr {
+  margin: 0;
+}
 </style>
