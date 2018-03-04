e4afeaa632cc321916d02f02d3b99ee421228088
Ben Garcia <ben@seebenprogram.com>
2018-03-03 22:54:42 -0500

    Add chat input component

--- src/components/play/chat/ChatInput.vue
+++ src/components/play/chat/ChatInput.vue
@@ -1,5 +1,26 @@
 <template>
-  
+  <form @submit.prevent="">
+    <div class="input-group">
+
+      <input 
+        placeholder="(kindly) taunt opponents here" 
+        type="text" 
+        class="form-control"
+      >
+
+      <div class="input-group-append">
+
+        <button 
+          @click.prevent=""
+          class="btn btn-outline-secondary" 
+          type="button"
+        >
+          Send
+        </button>
+
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
