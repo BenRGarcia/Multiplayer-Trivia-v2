80edbd291f3dfc83e7c6616d38a59e305f2c2e5d
Ben Garcia <ben@seebenprogram.com>
2018-03-03 21:17:59 -0500

    Add boilerplate vue template tags

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,14 @@
 </template>
 
 <script>
-export default {
+import Leaderboard from './home/Leaderboard'
+import Leader from './home/Leader'
 
+export default {
+  components: {
+    Leaderboard,
+    Leader
+  }
 }
 </script>
 
