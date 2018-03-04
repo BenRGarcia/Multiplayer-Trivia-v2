e8f467b7940b7ed9982a52c403262295e7c4b3de
Ben Garcia <ben@seebenprogram.com>
2018-03-03 20:42:43 -0500

    Add boilerplate code for vue/vuex store

---
+++ src/store/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueFire from 'vuefire'
+import firebase from '../firebase/firebase'
+
+// Vuex for centralized store for components
+Vue.use(Vuex);
+// Vuefire for vue/firebase bindings
+Vue.use(VueFire);
+// firebase db
+Vue.use(firebase);
+
+export const store = new Vuex.Store({
+  state: {
+
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
