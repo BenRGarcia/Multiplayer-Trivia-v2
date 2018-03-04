60fa68feb84eab7b07ab72b666a72d0fe59a530f
Ben Garcia <ben@seebenprogram.com>
2018-03-03 20:23:54 -0500

    Add imports for bootstrap, add 'store' to Vue 'el'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Vue
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { store } from './store'
+// Bootstrap
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Custom CSS
+import './assets/css/style.css'
+
+// Bootstrap enable tooltips/popovers
+$(function () {
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+})
 
 Vue.config.productionTip = false
 
@@ -10,6 +21,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
