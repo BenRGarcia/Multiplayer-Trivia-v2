aa6aa551bda35e28a9c6631d4943478bae7e4825
Ben Garcia <ben@seebenprogram.com>
2018-03-03 21:12:13 -0500

    Add router paths for home/play/admin pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/components/Home'
+import Play from '@/components/Play'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    /*{
-      path: '/',
-      name: 'Home',
-      component: HelloWorld
-    }*/
-  ]
+      { // Redirect unknown paths to home
+        path: '*',
+        redirect: '/home'
+      },
+      { // Home page
+        path: '/home',
+        name: 'Home',
+        component: Home,
+      },
+      { // Play page
+        path: '/play',
+        name: 'Play',
+        component: Play,
+      },
+      { // Admin page
+        path: '/admin',
+        name: 'Admin',
+        component: Admin,
+      }
+    ]
 })
