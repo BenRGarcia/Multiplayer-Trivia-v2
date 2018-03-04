d2783d4dbcf5fdc4aee12d491c8c5a5fe471e544
Ben Garcia <ben@seebenprogram.com>
2018-03-03 22:43:49 -0500

    Build player stats component, add name change modal

--- src/components/play/stats/Modal.vue
+++ src/components/play/stats/Modal.vue
@@ -1,10 +1,74 @@
 <template>
-  
+  <div>
+
+    <!-- Button to trigger modal, call method to autofocus -->
+    <button 
+      @click="autofocusModal" 
+      type="button" 
+      class="btn btn-primary mb-0" 
+      data-toggle="modal" 
+      data-target="#playerNameModal"
+    >
+      Change Player Name
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="playerNameModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Create a Player Name to play</h5>
+            <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+            <!-- Begin modal form -->
+            <form 
+              v-on:submit.prevent=""
+            >
+
+              <div class="form-group">
+                <label for="player-name" class="col-form-label">Player Name:</label>
+                
+                <!-- Input box for player name -->
+                <input 
+                  type="text" 
+                  class="form-control" 
+                  id="player-name" 
+                  placeholder="ex. QuizlyBear88"
+                >
+
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+
+            <!-- Submit button -->
+            <input 
+              @click.prevent="" 
+              type="submit" 
+              class="btn btn-primary"
+            >
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    // Focus modal to input, save the user a 'click'
+    autofocusModal() {
+      $('#playerNameModal').on('shown.bs.modal', () => {
+        $('#player-name').trigger('focus');
+      })
+    }
+  }
 }
 </script>
 
--- src/components/play/stats/Stats.vue
+++ src/components/play/stats/Stats.vue
@@ -1,10 +1,48 @@
 <template>
-  
+  <div class="card">
+    <div class="card-body pb-0">
+      <table class="table table-sm">
+
+        <thead class="thead-dark">
+          <tr>
+            <!-- Player Name -->
+            <th scope="col">{{  }}QuizlyBear</th>
+            <th scope="col">Stats</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <th scope="row">Ranking</th>
+            <!-- Player's Ranking -->
+            <td>{{  }}3</td>
+          </tr>
+          <tr scope="row">
+            <th>Points</th>
+            <!-- Player's Points -->
+            <td>{{  }}30</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+
+    <div class="card-footer text-muted">
+
+      <!-- 'Change Player Name' Modal -->
+      <Modal/>
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Modal from './Modal'
 
+export default {
+  components: {
+    Modal
+  }
 }
 </script>
 
