f58d9a211511239bc4b2a0edfd81c772bb79e1d6
Ben Garcia <ben@seebenprogram.com>
2018-03-03 20:44:34 -0500

    Import vuex store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { store } from './store'
+import { store } from './store/store'
 // Bootstrap
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
