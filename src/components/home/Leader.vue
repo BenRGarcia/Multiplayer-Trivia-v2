52adb85438ae441571f48963dd6de1b9084619ed
Ben Garcia <ben@seebenprogram.com>
2018-03-03 21:44:26 -0500

    Add layout for each leader of leaderboard, hard code initial state

--- src/components/home/Leader.vue
+++ src/components/home/Leader.vue
@@ -1,5 +1,18 @@
 <template>
-  
+  <tr>
+    <th scope="row">
+      <!-- {{ rank }} -->
+      1
+    </th>
+    <td>
+      <!-- {{ name }} -->
+      Ben
+    </td>
+    <td>
+      <!-- {{ points }} -->
+      50
+    </td>
+  </tr>
 </template>
 
 <script>
